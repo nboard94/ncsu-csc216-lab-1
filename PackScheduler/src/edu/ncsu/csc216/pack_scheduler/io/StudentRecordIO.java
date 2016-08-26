cda02a381b745092b7a5cac2d51c439ddc97ebc2
James Ticatic <James Ticatic@JamesTicatic-PC>
2016-08-26 12:00:50 -0400

    tried to fix readstudentrecords

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIO.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 
@@ -13,26 +14,23 @@ import edu.ncsu.csc216.pack_scheduler.user.Student;
 public class StudentRecordIO {
 
 	public static ArrayList<Student> readStudentRecords(String fileName) throws FileNotFoundException {
-
 		ArrayList<Student> md = new ArrayList<Student>();
 		File file = new File(fileName);
-		Scanner scan = new Scanner(file).useDelimiter(",");
-		while (scan.hasNextLine()) {
-			String firstName = scan.next();
-			String lastName = scan.next();
-			String id = scan.next();
-			String email = scan.next();
-			String hashPW = scan.next();
-			int maxCredots = scan.nextInt();
-			Student s = new Student(firstName,lastName,id,email,hashPW,maxCredots);
-			md.add(s);
+		Scanner scan = new Scanner (file);
+		try {
+			while (scan.hasNextLine()) {
+				processStudent(scan.nextLine());
+			}
 		}
+		catch (NoSuchElementException e) {
+			scan.close();
+			throw new IllegalArgumentException();
+		}	
 		scan.close();
 		return md;
 	}
 
 	public static void writeStudentRecords(String fileName, ArrayList<Student> studentDirectory) throws IOException {
-		
 		File file = new File(fileName);
 		FileWriter writ = new FileWriter(file);
 		for (int i = 0; i <  studentDirectory.size(); i++) {
@@ -42,4 +40,28 @@ public class StudentRecordIO {
 		
 	}
 
+	private static Student processStudent(String line) {
+		Scanner scan = new Scanner(line).useDelimiter(",");
+		String firstName = null;
+		String lastName = null;
+		String id = null;
+		String email = null;
+		String hashPW = null;
+		int maxCredits = 0;
+		try {
+			firstName = scan.next();
+			lastName = scan.next();
+			id = scan.next();
+			email = scan.next();
+			hashPW = scan.next();
+			maxCredits = scan.nextInt();	
+		}
+		catch (NoSuchElementException e) {
+			scan.close();
+			throw new IllegalArgumentException();
+		}
+		scan.close();
+		Student s = new Student(firstName,lastName,id,email,hashPW,maxCredits);
+		return s;
+	}
 }
